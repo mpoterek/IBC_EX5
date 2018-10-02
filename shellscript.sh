e7ac54c1ade8a8fecfee24b4a3fdfa083dd07312
Patricia Portmann <pportman@nd.edu>
2018-10-02 14:50:32 -0400

    Added part 2: highest wage earner, lowest wage earner, and women in the top 10

--- shellscript.sh
+++ shellscript.sh
@@ -2,8 +2,17 @@
 # usage: bash shellscript.sh *.csv
 
 
-# usage: bash shellscript.sh wages.csv
+# usage: bash shellscript.sh
 
-cut -d ',' -f 1,2 wages.csv | grep [fm] | tr ',' ' ' | sort -k1,1 -k2n | uniq
+cut -d ',' -f 1,2 wages.csv | grep [fm] | tr ',' ' ' | sort -k1,1 -k2n | 
+uniq > wages_sorted.txt
 
+echo "Lowest wage earner: " 
+cut -d ',' -f 1,2,4 wages.csv | grep [fm] | tr ',' ' ' | sort -k3,3 | head -n 1
 
+echo "Highest wage earner: "
+cut -d ',' -f 1,2,4 wages.csv | grep [fm] | tr ',' ' ' | sort -k3,3 | tail -n 1
+
+echo "Number of females in top ten earners: "
+cut -d ',' -f 1,2,4 wages.csv | grep [fm] | tr ',' ' ' | sort -k3,3 | 
+tail -n 10 | grep "female" | wc -l
