b18137e1fe1329c62bacc50f3db0b3648bddfd3f
Patricia Portmann <pportman@nd.edu>
2018-10-04 23:07:08 -0400

    Just put the exact command suggested on the exercise, should work for someone with bc functionality installed.

--- shellscript.sh
+++ shellscript.sh
@@ -3,11 +3,15 @@
 #4 October 2018
 #Exercise 5
 # usage: bash shellscript.sh
+# makes file with unique gender-yearsExperience combos
 cut -d ',' -f 1,2 wages.csv | tr ',' ' ' | sort -k1,1 -k2n > wages_sorted.txt
+#finds lowest wage earner
 echo "Lowest wage earner: " 
 cut -d ',' -f 1,2,4 wages.csv | tr ',' ' ' | sort -k3,3 | head -n 1 #| head -n 1
+#finds highest wage earner
 echo "Highest wage earner: "
 cut -d ',' -f 1,2,4 wages.csv | tr ',' ' ' | sort -k3,3  | tail -n 2 | head -n 1 #| tail -n 1
+#calculates number of females in top 10 earners
 echo "Number of females in top ten earners: "
 cut -d ',' -f 1,2,4 wages.csv | tr ',' ' ' | sort -k3,3 | tail -n 10 | grep "female" | wc -l #| grep "female"
 # find average wage of non-college graduates (12 school years) from wages.csv
@@ -21,4 +25,5 @@ echo "This is the average of all wages for college grads: "
 yes_college="$(cut -d ',' -f 3,4 wages.csv | grep '16' | cut -d ',' -f 2 | awk '{total += $1} END {print total/NR}')"
 echo $yes_college
 echo "The wage effect of graduating college: "
-echo "$yes_college-$no_college" | tr -d $'\r' | bc #Additional pipe to remove carriage return
+echo "$yes_college - $no_college" | bc
+# Additional pipe to remove carriage return
